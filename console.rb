142040f09efa8a6c5de6c99db3e3039057345247
DeepCFoxXx <learncode23@gmail.com>
2018-03-26 15:45:45 +0100

    film seeds added

--- console.rb
+++ console.rb
@@ -3,6 +3,7 @@ require_relative( 'models/customer.rb' )
 require_relative( 'models/film.rb' )
 
 Customer.delete_all()
+Film.delete_all()
 
 
 customer1 = Customer.new({'name' => 'Kara', 'funds' => 500.00})
@@ -16,3 +17,16 @@ customer3.save()
 
 customer4 = Customer.new({'name' => 'Amber', 'funds' => 20.00})
 customer4.save()
+
+
+film1 = Film.new({'title' => 'Dredd', 'price' => 10.00})
+film1.save
+
+film2 = Film.new({'title' => 'Blade Runner 2049', 'price' => 15.00})
+film2.save
+
+film3 = Film.new({'title' => 'Aliens', 'price' => 5.00})
+film3.save
+
+film4 = Film.new({'title' => 'Predator', 'price' => 20.00})
+film4.save
