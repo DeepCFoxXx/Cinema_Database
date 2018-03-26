2898abb1555994bb5a151b5ad61a5fdad2f3309f
DeepCFoxXx <learncode23@gmail.com>
2018-03-26 15:49:47 +0100

    ticket seeds added

--- console.rb
+++ console.rb
@@ -2,6 +2,7 @@ require_relative( 'models/ticket.rb' )
 require_relative( 'models/customer.rb' )
 require_relative( 'models/film.rb' )
 
+Ticket.delete_all()
 Customer.delete_all()
 Film.delete_all()
 
@@ -30,3 +31,25 @@ film3.save
 
 film4 = Film.new({'title' => 'Predator', 'price' => 20.00})
 film4.save
+
+
+ticket1 = Ticket.new({'customer_id' => customer1.id, 'film_id' => film1.id})
+ticket1.save
+
+ticket2 = Ticket.new({'customer_id' => customer1.id, 'film_id' => film2.id})
+ticket2.save
+
+ticket3 = Ticket.new({'customer_id' => customer2.id, 'film_id' => film2.id})
+ticket3.save
+
+ticket4 = Ticket.new({'customer_id' => customer3.id, 'film_id' => film2.id})
+ticket4.save
+
+ticket5 = Ticket.new({'customer_id' => customer3.id, 'film_id' => film3.id})
+ticket5.save
+
+ticket6 = Ticket.new({'customer_id' => customer3.id, 'film_id' => film4.id})
+ticket6.save
+
+ticket7 = Ticket.new({'customer_id' => customer4.id, 'film_id' => film4.id})
+ticket7.save
