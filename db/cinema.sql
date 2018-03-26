74b98bbfe20a504e48191e299fa646cc6dac5d0f
DeepCFoxXx <learncode23@gmail.com>
2018-03-26 13:12:54 +0100

    2nd table added

--- db/cinema.sql
+++ db/cinema.sql
@@ -1,7 +1,14 @@
+DROP TABLE customers;
 DROP TABLE films;
 
 CREATE TABLE films (
   id SERIAL8 PRIMARY KEY,
   title VARCHAR(255),
   price NUMERIC
+);
+
+CREATE TABLE customers (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  funds NUMERIC
 );
