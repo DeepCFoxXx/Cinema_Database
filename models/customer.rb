c9c01b849104bfc51c19ba26806827d4572430e8
DeepCFoxXx <learncode23@gmail.com>
2018-03-26 13:26:08 +0100

    save added

---
+++ models/customer.rb
@@ -0,0 +1,30 @@
+require_relative('../db/sql_runner.rb')
+
+class Customer
+
+  attr_reader :id
+  attr_accessor :name, :funds
+
+  def initialize(options)
+    @id = options['id'].to_i
+    @name = options['name']
+    @funds = options['funds'].to_f
+  end
+
+  def save()
+    sql = "INSERT INTO customers
+    (
+      name,
+      funds
+    )
+    VALUES
+    (
+      $1, $2
+    )
+    RETURNING *"
+    values = [@name, @funds]
+    customer = SqlRunner.run(sql, values).first
+    @id = customer['id'].to_i
+  end
+
+end   
