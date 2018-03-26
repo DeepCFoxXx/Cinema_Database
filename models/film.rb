5fe075043ee325f620aefcc7fcc6b10c7fa12b89
DeepCFoxXx <learncode23@gmail.com>
2018-03-26 16:20:09 +0100

    added update + refactored customers function

--- models/film.rb
+++ models/film.rb
@@ -26,12 +26,28 @@ class Film
     @id = film['id'].to_i
   end
 
+  def update()
+    sql = "UPDATE films
+    SET
+    (
+    title, price
+    ) =
+    (
+    $1, $2
+    )
+    WHERE id = $3"
+    values = [@title, @price, @id]
+    SqlRunner.run(sql, values)
+  end
+
   def customers()
     sql = "SELECT customers.*
     FROM customers
     INNER JOIN tickets
-    ON tickets.customer_id = customers.id
-    WHERE tickets.film_id = $1"
+    ON customers.id = tickets.customer_id
+    INNER JOIN screenings
+    ON tickets.screening_id = screenings.id
+    WHERE screenings.film_id = $1"
     values = [@id]
     customer_data = SqlRunner.run(sql, values)
     customers = Customer.map_items(customer_data)
